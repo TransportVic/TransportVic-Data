99b78ef4df3801d41dbe3615a3f184ef87812885
TransportVic <47815427+TransportVic@users.noreply.github.com>
2024-06-20 22:39:47 +1000

    fix geojson errors and include validity testing

---
+++ test/test-geojson-validity.mjs
@@ -0,0 +1,28 @@
+import path from 'path'
+import url from 'url'
+import { walkDir } from '../util.mjs'
+import { expect } from 'chai'
+import { readFile } from "fs/promises"
+import { isFeatureCollection } from "geojson-validation"
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const geospatialDir = path.join(__dirname, '..', 'geospatial')
+const files = await walkDir(geospatialDir)
+
+for (let file of files) {
+  // Exclude suburb boundaries file
+  if (file.endsWith('.geojson') && !(file.includes('suburb-boundaries'))) {
+    const relativePath = file.slice(geospatialDir.length + 1)
+    
+    describe(relativePath, () => {
+      it('Should be valid', async () => {
+        const fileData = JSON.parse(await readFile(file))
+        const errors = isFeatureCollection(fileData, true)
+        expect(errors).to.deep.equal([])
+      })
+    })
+  }
+}
+
