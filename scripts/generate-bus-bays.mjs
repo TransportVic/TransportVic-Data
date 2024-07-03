62962b18f79e847cd4f42f3eaccf5b34928e54a5
TransportVic <47815427+TransportVic@users.noreply.github.com>
2024-07-04 00:43:13 +1000

    move bus bays to csv as well

--- scripts/generate-bus-bays.mjs
+++ scripts/generate-bus-bays.mjs
@@ -1,19 +1,19 @@
-import readXLSXFile from 'read-excel-file/node'
 import { createReadStream } from 'fs'
 import { writeFile } from 'fs/promises'
 import path from 'path'
 import url from 'url'
+import CsvReadableStream from 'csv-reader'
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const busBaysDir = path.join(__dirname, '..', 'excel', 'bus', 'bays')
-const stream = createReadStream(path.join(busBaysDir, 'Bus Bays.xlsx'))
+const stream = createReadStream(path.join(busBaysDir, 'Bus Bays.csv'), 'utf-8')
 
-const rows = await readXLSXFile(stream)
-const output = rows.slice(1).reduce((acc, row) => {
-  acc[row[0]] = row[1]
-  return acc
-}, {})
-
-await writeFile(path.join(busBaysDir, 'bus-bays.json'), JSON.stringify(output))
+let acc = {}
+stream.pipe(new CsvReadableStream())
+  .on('data', row => {
+    acc[row[0]] = row[1]
+  }).on('end', async () => {
+    await writeFile(path.join(busBaysDir, 'bus-bays.json'), JSON.stringify(acc))
+  })
