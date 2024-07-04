daa381e1e450c7d37b14da08b0bd7d7e1ec43161
TransportVic <47815427+TransportVic@users.noreply.github.com>
2024-07-04 15:14:43 +1000

    move rail platforms to csv as well

--- scripts/generate-rail-platforms.mjs
+++ scripts/generate-rail-platforms.mjs
@@ -1,24 +1,23 @@
-import readXLSXFile from 'read-excel-file/node'
 import { createReadStream } from 'fs'
 import { writeFile } from 'fs/promises'
 import path from 'path'
 import url from 'url'
+import CsvReadableStream from 'csv-reader'
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const platformsDir = path.join(__dirname, '..', 'excel', 'rail', 'platform-side')
-const stream = createReadStream(path.join(platformsDir, 'Platforms.xlsx'))
+const stream = createReadStream(path.join(platformsDir, 'Platforms.csv'), 'utf-8')
 
-const rows = await readXLSXFile(stream)
-const output = rows.slice(1).reduce((acc, row) => {
-  let platforms = row.slice(1)
+let acc = {}
+stream.pipe(new CsvReadableStream())
+  .on('data', row => {
+    let platforms = row.slice(1)
 
-  let i = platforms.length - 1
-  for (; i > 0 && platforms[i] === null; i--);
-  acc[row[0]] = platforms.slice(0, i + 1)
-
-  return acc
-}, {})
-
-await writeFile(path.join(platformsDir, 'platforms.json'), JSON.stringify(output))
+    let i = platforms.length - 1
+    for (; i > 0 && platforms[i] === ''; i--);
+    acc[row[0]] = platforms.slice(0, i + 1)  
+  }).on('end', async () => {
+    await writeFile(path.join(platformsDir, 'platforms.json'), JSON.stringify(acc))
+  })
