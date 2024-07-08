89e7ca9a8332631c8cdb280e19af48ec5dd5f9dd
TransportVic <47815427+TransportVic@users.noreply.github.com>
2024-07-08 12:18:45 +1000

    calculate the door direction

---
+++ sample/get-doors.mjs
@@ -0,0 +1,42 @@
+import doors from '../excel/rail/platform-side/platforms.json' assert { type: 'json' }
+
+let easternLines = [
+  "Alamein", "Belgrave", "Glen Waverley", "Lilydale",
+  "Frankston", "Cranbourne", "Pakenham", "Sandringham",
+  "Mernda", "Hurstbridge", "City Circle"
+]
+
+function accountForDirection(tripDirection, doorSide) {
+  if (tripDirection === 'Down') return doorSide
+  return doorSide === 'L' ? 'R' : 'L'
+}
+
+function getFSSDirectionUp(trip) {
+  if (easternLines.includes(trip.routeData.routeName)) {
+    return trip.runData.viaCityLoop ? 'Up' : 'Down'
+  }
+
+  // Western
+  return trip.runData.viaCityLoop ? 'Down' : 'Up'
+}
+
+function getFSSDirection(trip) {
+  let upDirection = getFSSDirectionUp(trip)
+  if (trip.runData.direction.railDirection === 'Up') return upDirection
+
+  return upDirection === 'Up' ? 'Down' : 'Up'
+}
+
+export default function appendDoorsData(trip) {
+  trip.stops.forEach(stop => {
+    let platformInt = parseInt(stop.platform) - 1
+    if (!doors[stop.stationName]) console.log(stop.stationName)
+    let rawDoorSide = doors[stop.stationName][platformInt]
+
+    let direction = trip.runData.direction.railDirection
+    if (stop.stationName === 'Flinders Street') direction = getFSSDirection(trip)
+
+    stop.doorSide = accountForDirection(direction, rawDoorSide)
+  })
+}
+
---
+++ test/test-doors.mjs
@@ -0,0 +1,57 @@
+import { expect } from 'chai'
+import LIL_FSS_VLP from './doors-pattern/lil-fss-vlp.json' assert { type: 'json' }
+import MDD_FSS_DIR from './doors-pattern/mdd-fss-direct.json' assert { type: 'json' }
+import FSS_BEG_DIR from './doors-pattern/fss-beg-direct.json' assert { type: 'json' }
+import SUY_FSS_SSS from './doors-pattern/suy-fss-sss.json' assert { type: 'json' }
+import FSS_HBE_VLP from './doors-pattern/fss-hbe-vlp.json' assert { type: 'json' }
+import CCL_VLP from './doors-pattern/ccl-vlp.json' assert { type: 'json' }
+
+import appendDoorsData from '../sample/get-doors.mjs'
+
+describe('The door side calculation', () => {
+  appendDoorsData(LIL_FSS_VLP)
+  appendDoorsData(MDD_FSS_DIR)
+  appendDoorsData(FSS_BEG_DIR)
+  appendDoorsData(SUY_FSS_SSS)
+  appendDoorsData(FSS_HBE_VLP)
+  appendDoorsData(CCL_VLP)
+
+  it('Should reverse the door side for Up trips', () => {
+    expect(LIL_FSS_VLP.stops[0].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stops[3].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stops[6].doorSide).to.equal('R')
+    expect(LIL_FSS_VLP.stops[11].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stops[12].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stops[15].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stops[16].doorSide).to.equal('R')
+  })
+
+  it('Should handle Flinders Street specially, treating Westbound travel as Down', () => {
+    expect(MDD_FSS_DIR.stops[0].doorSide).to.equal('R')
+    expect(MDD_FSS_DIR.stops[23].doorSide).to.equal('L')
+    expect(MDD_FSS_DIR.stops[24].doorSide).to.equal('R')
+  })
+
+  it('Down Belgrave direct from FSS - RMD', () => {
+    expect(FSS_BEG_DIR.stops[0].doorSide).to.equal('R')
+  })
+
+  it('Up Sunbury direct from NME - SSS - FSS', () => {
+    expect(SUY_FSS_SSS.stops[12].doorSide).to.equal('L')
+    expect(SUY_FSS_SSS.stops[13].doorSide).to.equal('R')
+    expect(SUY_FSS_SSS.stops[14].doorSide).to.equal('L')
+  })
+
+  it('Down Hurstbridge via loop', () => {
+    expect(FSS_HBE_VLP.stops[0].doorSide).to.equal('R')
+    expect(FSS_HBE_VLP.stops[1].doorSide).to.equal('L')
+    expect(FSS_HBE_VLP.stops[2].doorSide).to.equal('L')
+    expect(FSS_HBE_VLP.stops[24].doorSide).to.equal('L')
+  })
+
+  it('Down city circle via Southern Cross', () => {
+    expect(CCL_VLP.stops[0].doorSide).to.equal('L')
+    expect(CCL_VLP.stops[1].doorSide).to.equal('L')
+    expect(CCL_VLP.stops[5].doorSide).to.equal('L')
+  })
+})
