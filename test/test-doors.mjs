27cbbfda2db7587034912fa398cb1b80894fd7d6
TransportVic <47815427+TransportVic@users.noreply.github.com>
2024-07-08 14:44:59 +1000

    more western lines test

--- test/test-doors.mjs
+++ test/test-doors.mjs
@@ -3,8 +3,10 @@ import LIL_FSS_VLP from './doors-pattern/lil-fss-vlp.json' assert { type: 'json'
 import MDD_FSS_DIR from './doors-pattern/mdd-fss-direct.json' assert { type: 'json' }
 import FSS_BEG_DIR from './doors-pattern/fss-beg-direct.json' assert { type: 'json' }
 import SUY_FSS_SSS from './doors-pattern/suy-fss-sss.json' assert { type: 'json' }
+import SUY_FSS_VLP from './doors-pattern/suy-fss-vlp.json' assert { type: 'json' }
 import FSS_HBE_VLP from './doors-pattern/fss-hbe-vlp.json' assert { type: 'json' }
 import CCL_VLP from './doors-pattern/ccl-vlp.json' assert { type: 'json' }
+import FSS_SUY_VLP from './doors-pattern/fss-suy-vlp.json' assert { type: 'json' }
 
 import appendDoorsData from '../sample/get-doors.mjs'
 
@@ -13,8 +15,10 @@ describe('The door side calculation', () => {
   appendDoorsData(MDD_FSS_DIR)
   appendDoorsData(FSS_BEG_DIR)
   appendDoorsData(SUY_FSS_SSS)
+  appendDoorsData(SUY_FSS_VLP)
   appendDoorsData(FSS_HBE_VLP)
   appendDoorsData(CCL_VLP)
+  appendDoorsData(FSS_SUY_VLP)
 
   it('Should reverse the door side for Up trips', () => {
     expect(LIL_FSS_VLP.stops[0].doorSide).to.equal('L')
@@ -53,5 +57,18 @@ describe('The door side calculation', () => {
     expect(CCL_VLP.stops[0].doorSide).to.equal('L')
     expect(CCL_VLP.stops[1].doorSide).to.equal('L')
     expect(CCL_VLP.stops[5].doorSide).to.equal('L')
+  })
+
+  it('Down Sunbury via loop', () => {
+    expect(FSS_SUY_VLP.stops[0].doorSide).to.equal('L')
+    expect(FSS_SUY_VLP.stops[1].doorSide).to.equal('R')
+    expect(FSS_SUY_VLP.stops[5].doorSide).to.equal('L')
+  })
+
+  it('Up Sunbury via loop', () => {
+    expect(SUY_FSS_VLP.stops[0].doorSide).to.equal('R')
+    expect(SUY_FSS_VLP.stops[12].doorSide).to.equal('L')
+    expect(SUY_FSS_VLP.stops[13].doorSide).to.equal('L')
+    expect(SUY_FSS_VLP.stops[16].doorSide).to.equal('R')
   })
 })
