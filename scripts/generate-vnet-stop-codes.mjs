21f548bede982679d7cd618b9815e76211490c51
leveson <145632757+leveson@users.noreply.github.com>
2024-10-27 09:44:40 +1100

    add vnet data
    1. add new data: vnet codes for vline stops/stations.
    2. add new generation functionality: csv to json for vline station data.
    
    Signed-off-by: leveson <145632757+leveson@users.noreply.github.com>

---
+++ scripts/generate-vnet-stop-codes.mjs
@@ -0,0 +1,31 @@
+import { createReadStream } from 'fs'
+import { writeFile } from 'fs/promises'
+import path from 'path'
+import url from 'url'
+import CsvReadableStream from 'csv-reader'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const vnetDir = path.join(__dirname, '..', 'excel', 'vnet')
+const vnetBookableCodesStream = createReadStream(path.join(vnetDir, 'VNet Bookable Codes.csv'), 'utf-8')
+const vnetOverridesStream = createReadStream(path.join(vnetDir, 'VNet Overrides.csv'), 'utf-8')
+
+let acc = {}
+let overrides = new Set()
+
+vnetOverridesStream.pipe(new CsvReadableStream({ asObject: true }))
+  .on('data', row => {
+    overrides.add(row.stop_name)
+  })
+  .on('end', () => {
+    vnetBookableCodesStream.pipe(new CsvReadableStream({ asObject: true }))
+      .on('data', row => {
+        if (!overrides.has(row.stop_name)) {
+          acc[row.stop_name] = row.vnet_code
+        }
+      })
+      .on('end', async () => {
+        await writeFile(path.join(vnetDir, 'vnet-stop-codes.json'), JSON.stringify(acc))
+      })
+  })
