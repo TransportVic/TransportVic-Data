599a6ea894a047e38fc7f110cf2f28eebb64ee0e
TransportVic <47815427+TransportVic@users.noreply.github.com>
2025-01-20 20:14:37 +1100

    added corresponding csv

---
+++ scripts/generate-vline-route-names.mjs
@@ -0,0 +1,20 @@
+import { createReadStream } from 'fs'
+import { writeFile } from 'fs/promises'
+import path from 'path'
+import url from 'url'
+import CsvReadableStream from 'csv-reader'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const routesDir = path.join(__dirname, '..', 'excel', 'rail', 'vline-route-names')
+const stream = createReadStream(path.join(routesDir, 'Routes.csv'), 'utf-8')
+
+let acc = {}
+
+stream.pipe(new CsvReadableStream({ asObject: true }))
+  .on('data', row => {
+    acc[row.route_gtfs_id] = row.route_name
+  }).on('end', async () => {
+    await writeFile(path.join(routesDir, 'routes.json'), JSON.stringify(acc, null, 2))
+  })
