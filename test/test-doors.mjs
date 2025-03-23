116ffdbfc634cd74c77281fb197701033a2e4573
TransportVic <47815427+TransportVic@users.noreply.github.com>
2025-03-23 21:55:24 +1100

    added MTP door side

--- test/test-doors.mjs
+++ test/test-doors.mjs
@@ -7,6 +7,8 @@ import SUY_FSS_VLP from './doors-pattern/suy-fss-vlp.json' with { type: 'json' }
 import FSS_HBE_VLP from './doors-pattern/fss-hbe-vlp.json' with { type: 'json' }
 import CCL_VLP from './doors-pattern/ccl-vlp.json' with { type: 'json' }
 import FSS_SUY_VLP from './doors-pattern/fss-suy-vlp.json' with { type: 'json' }
+import MTP_ANZ_THL from './doors-pattern/mtp-anz-thl.json' with { type: 'json' }
+import MTP_THL_AEN from './doors-pattern/mtp-thl-aen.json' with { type: 'json' }
 
 import appendDoorsData from '../sample/get-doors.mjs'
 
@@ -19,6 +21,8 @@ describe('The door side calculation', () => {
   appendDoorsData(FSS_HBE_VLP)
   appendDoorsData(CCL_VLP)
   appendDoorsData(FSS_SUY_VLP)
+  appendDoorsData(MTP_ANZ_THL)
+  appendDoorsData(MTP_THL_AEN)
 
   it('Should reverse the door side for Up trips', () => {
     expect(LIL_FSS_VLP.stops[0].doorSide).to.equal('L')
@@ -70,5 +74,19 @@ describe('The door side calculation', () => {
     expect(SUY_FSS_VLP.stops[12].doorSide).to.equal('L')
     expect(SUY_FSS_VLP.stops[13].doorSide).to.equal('L')
     expect(SUY_FSS_VLP.stops[16].doorSide).to.equal('R')
+  })
+
+  it('Hawksburn to Town Hall', () => {
+    expect(MTP_ANZ_THL.stops[0].doorSide).to.equal('L')
+    expect(MTP_ANZ_THL.stops[1].doorSide).to.equal('R')
+    expect(MTP_ANZ_THL.stops[2].doorSide).to.equal('R')
+  })
+
+  it('Town Hall to West Footscray', () => {
+    expect(MTP_THL_AEN.stops[0].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stops[1].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stops[2].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stops[3].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stops[4].doorSide).to.equal('R')
   })
 })
