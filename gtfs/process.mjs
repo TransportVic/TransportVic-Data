ee27865a459ff0b8ae6245a3c1fac6079ca2ca61
TransportVic <47815427+TransportVic@users.noreply.github.com>
2025-08-10 19:34:05 +1000

    implement run id stuff

--- gtfs/process.mjs
+++ gtfs/process.mjs
@@ -185,6 +185,12 @@ export async function createTripProcessor(database) {
       return trip
     },
     10: function processTrip(trip) {
+      const dayOfWeekRaw = dateUtils.parseDate(trip.operationDays[0]).weekday
+      const dayOfWeek = dayOfWeekRaw === 7 ? 1 : dayOfWeekRaw + 1
+      const sssBound = trip.destination === 'Southern Cross Railway Station'
+
+      trip.runID = `${dayOfWeek}${sssBound ? 'AM' : 'MA'}8`
+
       return trip
     }
   }
