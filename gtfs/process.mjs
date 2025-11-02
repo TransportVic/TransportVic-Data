19414eaacd244180114b0a74219250f0234bd7cb
TransportVic <47815427+TransportVic@users.noreply.github.com>
2025-11-02 13:35:48 +1100

    include W88 NNG-TRN

--- gtfs/process.mjs
+++ gtfs/process.mjs
@@ -44,7 +44,7 @@ export function createRouteProcessor() {
       return route
     },
     4: function processRoute(route) {
-      if (route.routeGTFSID.match(/4-w\d\d/) || (route.routeGTFSID.match(/4-V\d\d/))) {
+      if (route.routeGTFSID.match(/4-w\d\d/) || route.routeGTFSID.match(/4-V\d\d/) || route.routeGTFSID === '4-W88') {
         route.routeGTFSID = '4-WGT'
         route.routeName = 'West Gippsland Transit'
       }
