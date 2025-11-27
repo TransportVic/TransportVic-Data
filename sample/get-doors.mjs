6c97c120e0bcebb0b78913f410c79cc8b42d16f1
TransportVic <47815427+TransportVic@users.noreply.github.com>
2025-11-27 22:20:53 +0800

    fix up door side code

--- sample/get-doors.mjs
+++ sample/get-doors.mjs
@@ -16,37 +16,41 @@ function accountForDirection(tripDirection, doorSide) {
 }
 
 function getFSSDirectionUp(trip) {
-  if (easternLines.includes(trip.routeData.routeName)) {
-    return trip.runData.viaCityLoop ? 'Up' : 'Down'
+  const viaLoop = trip.stopTimings.some(stop => stop.stopName === 'Parliament Railway Station' && !stop.cancelled)
+
+  if (easternLines.includes(trip.routeName)) {
+    return viaLoop ? 'Up' : 'Down'
   }
 
   // Western
-  return trip.runData.viaCityLoop ? 'Down' : 'Up'
+  return viaLoop ? 'Down' : 'Up'
 }
 
 function getFSSDirection(trip) {
   let upDirection = getFSSDirectionUp(trip)
-  if (trip.runData.direction.railDirection === 'Up') return upDirection
+  if (trip.direction === 'Up') return upDirection
 
   return upDirection === 'Up' ? 'Down' : 'Up'
 }
 
 function getTHLDirection(trip) {
-  let upDirection = mtpEasternLines.includes(trip.routeData.routeName) ? 'Down' : 'Up'
-  if (trip.runData.direction.railDirection === 'Up') return upDirection
+  let upDirection = mtpEasternLines.includes(trip.routeName) ? 'Down' : 'Up'
+  if (trip.direction === 'Up') return upDirection
 
   return upDirection === 'Up' ? 'Down' : 'Up'
 }
 
 export default function appendDoorsData(trip) {
-  trip.stops.forEach(stop => {
+  trip.stopTimings.forEach(stop => {
     let platformInt = parseInt(stop.platform) - 1
-    if (!doors[stop.stationName]) console.log(stop.stationName)
-    let rawDoorSide = doors[stop.stationName][platformInt]
+    const stopName = stop.stopName.slice(0, -16)
 
-    let direction = trip.runData.direction.railDirection
-    if (stop.stationName === 'Flinders Street') direction = getFSSDirection(trip)
-    if (stop.stationName === 'Town Hall') direction = getTHLDirection(trip)
+    if (!doors[stopName]) console.log(stopName)
+    let rawDoorSide = doors[stopName][platformInt]
+
+    let direction = trip.direction
+    if (stopName === 'Flinders Street') direction = getFSSDirection(trip)
+    if (stopName === 'Town Hall') direction = getTHLDirection(trip)
 
     stop.doorSide = accountForDirection(direction, rawDoorSide)
   })
--- test/test-doors.mjs
+++ test/test-doors.mjs
@@ -10,20 +10,22 @@ import FSS_SUY_VLP from './doors-pattern/fss-suy-vlp.json' with { type: 'json' }
 import MTP_ANZ_THL from './doors-pattern/mtp-anz-thl.json' with { type: 'json' }
 import MTP_THL_AEN from './doors-pattern/mtp-thl-aen.json' with { type: 'json' }
 
-const MTP_ANZ_THL_2 = JSON.parse(JSON.stringify(MTP_ANZ_THL))
-const MTP_THL_AEN_2 = JSON.parse(JSON.stringify(MTP_THL_AEN))
-MTP_ANZ_THL_2.stops.slice(1).forEach(stop => stop.platform = '2')
-MTP_THL_AEN_2.stops.slice(0, -1).forEach(stop => stop.platform = '2')
-
-const MTP_AEN_THL = JSON.parse(JSON.stringify(MTP_THL_AEN))
-MTP_AEN_THL.stops.reverse()
-MTP_AEN_THL.runData.direction.railDirection = 'Up'
-
-const MTP_THL_ANZ = JSON.parse(JSON.stringify(MTP_ANZ_THL))
-MTP_THL_ANZ.stops.reverse()
-MTP_THL_ANZ.runData.direction.railDirection = 'Down'
-
 import appendDoorsData from '../sample/get-doors.mjs'
+
+const clone = o => JSON.parse(JSON.stringify(o))
+
+const MTP_ANZ_THL_2 = clone(MTP_ANZ_THL)
+const MTP_THL_AEN_2 = clone(MTP_THL_AEN)
+MTP_ANZ_THL_2.stopTimings.slice(1).forEach(stop => stop.platform = '2')
+MTP_THL_AEN_2.stopTimings.slice(0, -1).forEach(stop => stop.platform = '2')
+
+const MTP_AEN_THL = clone(MTP_THL_AEN)
+MTP_AEN_THL.stopTimings.reverse()
+MTP_AEN_THL.direction = 'Up'
+
+const MTP_THL_ANZ = clone(MTP_ANZ_THL)
+MTP_THL_ANZ.stopTimings.reverse()
+MTP_THL_ANZ.direction = 'Down'
 
 describe('The door side calculation', () => {
   appendDoorsData(LIL_FSS_VLP)
@@ -44,97 +46,97 @@ describe('The door side calculation', () => {
   appendDoorsData(MTP_THL_ANZ)
 
   it('Should reverse the door side for Up trips', () => {
-    expect(LIL_FSS_VLP.stops[0].doorSide).to.equal('L')
-    expect(LIL_FSS_VLP.stops[3].doorSide).to.equal('L')
-    expect(LIL_FSS_VLP.stops[6].doorSide).to.equal('R')
-    expect(LIL_FSS_VLP.stops[11].doorSide).to.equal('L')
-    expect(LIL_FSS_VLP.stops[12].doorSide).to.equal('L')
-    expect(LIL_FSS_VLP.stops[15].doorSide).to.equal('L')
-    expect(LIL_FSS_VLP.stops[16].doorSide).to.equal('R')
+    expect(LIL_FSS_VLP.stopTimings[0].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stopTimings[3].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stopTimings[6].doorSide).to.equal('R')
+    expect(LIL_FSS_VLP.stopTimings[11].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stopTimings[12].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stopTimings[15].doorSide).to.equal('L')
+    expect(LIL_FSS_VLP.stopTimings[16].doorSide).to.equal('R')
   })
 
   it('Should handle Flinders Street specially, treating Westbound travel as Down', () => {
-    expect(MDD_FSS_DIR.stops[0].doorSide).to.equal('R')
-    expect(MDD_FSS_DIR.stops[23].doorSide).to.equal('L')
-    expect(MDD_FSS_DIR.stops[24].doorSide).to.equal('R')
+    expect(MDD_FSS_DIR.stopTimings[0].doorSide).to.equal('R')
+    expect(MDD_FSS_DIR.stopTimings[23].doorSide).to.equal('L')
+    expect(MDD_FSS_DIR.stopTimings[24].doorSide).to.equal('R')
   })
 
   it('Down Belgrave direct from FSS - RMD', () => {
-    expect(FSS_BEG_DIR.stops[0].doorSide).to.equal('R')
+    expect(FSS_BEG_DIR.stopTimings[0].doorSide).to.equal('R')
   })
 
   it('Up Sunbury direct from NME - SSS - FSS', () => {
-    expect(SUY_FSS_SSS.stops[12].doorSide).to.equal('L')
-    expect(SUY_FSS_SSS.stops[13].doorSide).to.equal('R')
-    expect(SUY_FSS_SSS.stops[14].doorSide).to.equal('L')
+    expect(SUY_FSS_SSS.stopTimings[12].doorSide).to.equal('L')
+    expect(SUY_FSS_SSS.stopTimings[13].doorSide).to.equal('R')
+    expect(SUY_FSS_SSS.stopTimings[14].doorSide).to.equal('L')
   })
 
   it('Down Hurstbridge via loop', () => {
-    expect(FSS_HBE_VLP.stops[0].doorSide).to.equal('R')
-    expect(FSS_HBE_VLP.stops[1].doorSide).to.equal('L')
-    expect(FSS_HBE_VLP.stops[2].doorSide).to.equal('L')
-    expect(FSS_HBE_VLP.stops[24].doorSide).to.equal('L')
+    expect(FSS_HBE_VLP.stopTimings[0].doorSide).to.equal('R')
+    expect(FSS_HBE_VLP.stopTimings[1].doorSide).to.equal('L')
+    expect(FSS_HBE_VLP.stopTimings[2].doorSide).to.equal('L')
+    expect(FSS_HBE_VLP.stopTimings[24].doorSide).to.equal('L')
   })
 
   it('Down city circle via Southern Cross', () => {
-    expect(CCL_VLP.stops[0].doorSide).to.equal('L')
-    expect(CCL_VLP.stops[1].doorSide).to.equal('L')
-    expect(CCL_VLP.stops[5].doorSide).to.equal('L')
+    expect(CCL_VLP.stopTimings[0].doorSide).to.equal('L')
+    expect(CCL_VLP.stopTimings[1].doorSide).to.equal('L')
+    expect(CCL_VLP.stopTimings[5].doorSide).to.equal('L')
   })
 
   it('Down Sunbury via loop', () => {
-    expect(FSS_SUY_VLP.stops[0].doorSide).to.equal('L')
-    expect(FSS_SUY_VLP.stops[1].doorSide).to.equal('R')
-    expect(FSS_SUY_VLP.stops[5].doorSide).to.equal('L')
+    expect(FSS_SUY_VLP.stopTimings[0].doorSide).to.equal('L')
+    expect(FSS_SUY_VLP.stopTimings[1].doorSide).to.equal('R')
+    expect(FSS_SUY_VLP.stopTimings[5].doorSide).to.equal('L')
   })
 
   it('Up Sunbury via loop', () => {
-    expect(SUY_FSS_VLP.stops[0].doorSide).to.equal('R')
-    expect(SUY_FSS_VLP.stops[12].doorSide).to.equal('L')
-    expect(SUY_FSS_VLP.stops[13].doorSide).to.equal('L')
-    expect(SUY_FSS_VLP.stops[16].doorSide).to.equal('R')
+    expect(SUY_FSS_VLP.stopTimings[0].doorSide).to.equal('R')
+    expect(SUY_FSS_VLP.stopTimings[12].doorSide).to.equal('L')
+    expect(SUY_FSS_VLP.stopTimings[13].doorSide).to.equal('L')
+    expect(SUY_FSS_VLP.stopTimings[16].doorSide).to.equal('R')
   })
 
   it('Hawksburn to Town Hall', () => {
-    expect(MTP_ANZ_THL.stops[0].doorSide).to.equal('L')
-    expect(MTP_ANZ_THL.stops[1].doorSide).to.equal('R')
-    expect(MTP_ANZ_THL.stops[2].doorSide).to.equal('R')
+    expect(MTP_ANZ_THL.stopTimings[0].doorSide).to.equal('L')
+    expect(MTP_ANZ_THL.stopTimings[1].doorSide).to.equal('R')
+    expect(MTP_ANZ_THL.stopTimings[2].doorSide).to.equal('R')
   })
 
   it('Town Hall to West Footscray', () => {
-    expect(MTP_THL_AEN.stops[0].doorSide).to.equal('R')
-    expect(MTP_THL_AEN.stops[1].doorSide).to.equal('R')
-    expect(MTP_THL_AEN.stops[2].doorSide).to.equal('R')
-    expect(MTP_THL_AEN.stops[3].doorSide).to.equal('R')
-    expect(MTP_THL_AEN.stops[4].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stopTimings[0].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stopTimings[1].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stopTimings[2].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stopTimings[3].doorSide).to.equal('R')
+    expect(MTP_THL_AEN.stopTimings[4].doorSide).to.equal('R')
   })
 
   it('Hawksburn to Town Hall using P2', () => {
-    expect(MTP_ANZ_THL_2.stops[0].doorSide).to.equal('L')
-    expect(MTP_ANZ_THL_2.stops[1].doorSide).to.equal('L')
-    expect(MTP_ANZ_THL_2.stops[2].doorSide).to.equal('L')
+    expect(MTP_ANZ_THL_2.stopTimings[0].doorSide).to.equal('L')
+    expect(MTP_ANZ_THL_2.stopTimings[1].doorSide).to.equal('L')
+    expect(MTP_ANZ_THL_2.stopTimings[2].doorSide).to.equal('L')
   })
 
   it('Town Hall to West Footscray using P2', () => {
-    expect(MTP_THL_AEN_2.stops[0].doorSide).to.equal('L')
-    expect(MTP_THL_AEN_2.stops[1].doorSide).to.equal('L')
-    expect(MTP_THL_AEN_2.stops[2].doorSide).to.equal('L')
-    expect(MTP_THL_AEN_2.stops[3].doorSide).to.equal('L')
-    expect(MTP_THL_AEN_2.stops[4].doorSide).to.equal('R')
+    expect(MTP_THL_AEN_2.stopTimings[0].doorSide).to.equal('L')
+    expect(MTP_THL_AEN_2.stopTimings[1].doorSide).to.equal('L')
+    expect(MTP_THL_AEN_2.stopTimings[2].doorSide).to.equal('L')
+    expect(MTP_THL_AEN_2.stopTimings[3].doorSide).to.equal('L')
+    expect(MTP_THL_AEN_2.stopTimings[4].doorSide).to.equal('R')
   })
 
 
   it('West Footscray to Town Hall', () => {
-    expect(MTP_AEN_THL.stops[0].doorSide).to.equal('L')
-    expect(MTP_AEN_THL.stops[1].doorSide).to.equal('L')
-    expect(MTP_AEN_THL.stops[2].doorSide).to.equal('L')
-    expect(MTP_AEN_THL.stops[3].doorSide).to.equal('L')
-    expect(MTP_AEN_THL.stops[4].doorSide).to.equal('L')
+    expect(MTP_AEN_THL.stopTimings[0].doorSide).to.equal('L')
+    expect(MTP_AEN_THL.stopTimings[1].doorSide).to.equal('L')
+    expect(MTP_AEN_THL.stopTimings[2].doorSide).to.equal('L')
+    expect(MTP_AEN_THL.stopTimings[3].doorSide).to.equal('L')
+    expect(MTP_AEN_THL.stopTimings[4].doorSide).to.equal('L')
   })
 
   it('Town Hall to Hawksburn', () => {
-    expect(MTP_THL_ANZ.stops[0].doorSide).to.equal('L')
-    expect(MTP_THL_ANZ.stops[1].doorSide).to.equal('L')
-    expect(MTP_THL_ANZ.stops[2].doorSide).to.equal('R')
+    expect(MTP_THL_ANZ.stopTimings[0].doorSide).to.equal('L')
+    expect(MTP_THL_ANZ.stopTimings[1].doorSide).to.equal('L')
+    expect(MTP_THL_ANZ.stopTimings[2].doorSide).to.equal('R')
   })
 })
