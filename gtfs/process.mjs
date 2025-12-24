6f8dc40b9e2ade93f0b5c711610929c9feba657f
TransportVic <47815427+TransportVic@users.noreply.github.com>
2025-12-24 21:50:11 +0800

    unset route number for west gippsland transit

--- gtfs/process.mjs
+++ gtfs/process.mjs
@@ -48,6 +48,7 @@ export function createRouteProcessor() {
       if (route.routeGTFSID.match(/4-w\d\d/) || route.routeGTFSID.match(/4-V\d\d/) || route.routeGTFSID === '4-W88') {
         route.routeGTFSID = '4-WGT'
         route.routeName = 'West Gippsland Transit'
+        route.routeNumber = null
       }
 
       let parts
