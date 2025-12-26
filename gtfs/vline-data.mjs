b3054219cc1e0663c9e4b5ce3036aa554f316f22
TransportVic <47815427+TransportVic@users.noreply.github.com>
2025-12-26 18:55:39 +1100

    add springhurst fix

---
+++ gtfs/vline-data.mjs
@@ -0,0 +1,22 @@
+export default {
+  "stops": [
+    {
+      "stop_id": "vic:rail:SHT",
+      "stop_name": "Springhurst Railway Station (Springhurst)",
+      "stop_lat": "-36.1858934",
+      "stop_lon": "146.47041695",
+      "parent_station": "",
+      "location_type": "1",
+      "platform_code": ""
+    },
+    {
+      "stop_id": "20345",
+      "stop_name": "Springhurst Railway Station (Springhurst)",
+      "stop_lat": "-36.18549264",
+      "stop_lon": "146.46949658",
+      "parent_station": "vic:rail:SHT",
+      "location_type": "",
+      "platform_code": ""
+    }
+  ]
+}
