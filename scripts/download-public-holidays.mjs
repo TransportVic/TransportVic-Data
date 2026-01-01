d5062a97968d566291934db899d748bc7bd155a1
TransportVic <47815427+TransportVic@users.noreply.github.com>
2026-01-01 20:47:16 +1100

    add script to auto update calendar data

---
+++ scripts/download-public-holidays.mjs
@@ -0,0 +1,31 @@
+import { DOMParser } from '@xmldom/xmldom'
+import fs from 'fs/promises'
+import fetch from 'node-fetch'
+import path from 'path'
+import url from 'url'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const calendarDir = path.join(__dirname, '..', 'calendar', 'vic-public-holidays')
+
+const ICAL_URL_BASE = 'https://www.vic.gov.au/'
+const ICAL_URL_PATH = '/ical'
+
+const webpage = await (await fetch(new URL(ICAL_URL_PATH, ICAL_URL_BASE))).text()
+
+const dom = new DOMParser().parseFromString(webpage)
+const anchorTags = dom.documentElement.getElementsByTagName('a')
+const holidayLink = Array.from(anchorTags).find(tag => tag.textContent.includes('public holiday'))
+
+const linkPath = holidayLink.getAttribute('href')
+
+const fullPath = new URL(linkPath, ICAL_URL_BASE)
+
+const icalData = await (await fetch(fullPath)).text()
+await fs.writeFile(path.join(calendarDir, 'vic-holidays.ics'), icalData)
+
+await fs.writeFile(path.join(calendarDir, 'note.txt'), `Data is sourced from https://www.vic.gov.au/ical
+Last updated: ${new Date().toLocaleString()}`)
+
+process.exit(0)
