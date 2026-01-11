0d6a1734aceeb31233de42158b39efd153a3ac72
TransportVic <47815427+TransportVic@users.noreply.github.com>
2026-01-11 18:12:43 +1100

    add warrnambool data

--- rail-pdfs/scripts.mjs
+++ rail-pdfs/scripts.mjs
@@ -10,10 +10,18 @@ export default {
       ...resolvePaths([
         'geelong-west-tarneit/100-Geelong-Waurn-Ponds-temp-timetable-web-12-160126-v1.pdf',
         'geelong-west-tarneit/100-Geelong-Waurn-Ponds-temp-timetable-web-17-180126-v1.pdf',
-        'geelong-west-tarneit/100-Geelong-Waurn-Ponds-temp-timetable-web-19-230126-v1.pdf',
+        'geelong-west-tarneit/100-Geelong-Waurn-Ponds-temp-timetable-web-19-230126-v1.pdf'
+      ])
+    ], [
+      '--shift-op-day', '--default-month=January',
+      ...resolvePaths([
         'geelong-west-tarneit/100-Geelong-Waurn-Ponds-v-Werribee-temp-timetable-web-12-160126-v1.pdf',
         'geelong-west-tarneit/100-Geelong-Waurn-Ponds-v-Werribee-temp-timetable-web-19-230126-v1.pdf',
-        'geelong-west-tarneit/100-Geelong-Waurn-Ponds-v-Werribee-temp-timetable-web-110126-v1.pdf',
+        'geelong-west-tarneit/100-Geelong-Waurn-Ponds-v-Werribee-temp-timetable-web-110126-v1.pdf'
+      ])
+    ], [
+      '--shift-op-day', '--default-month=January',
+      ...resolvePaths([
         'geelong-west-tarneit/100-Tarneit-temp-timetable-web-12-160126-v1.pdf',
         'geelong-west-tarneit/100-Tarneit-temp-timetable-web-17-180126-v1.pdf',
         'geelong-west-tarneit/100-Tarneit-temp-timetable-web-19-230126-v1.pdf'
@@ -24,12 +32,18 @@ export default {
       resolvePaths(['geelong-west-tarneit/100-Geelong-Waurn-Ponds-v-Werribee-temp-timetable-web-17-180126-v1.pdf'])[0]
     ],
     [
-      resolvePaths([
+      ...resolvePaths([
         'ballarat-melton-upgrade/307-Maryborough-temp-timetable-web-12-160126-v1.pdf',
         'ballarat-melton-upgrade/307-Maryborough-temp-timetable-web-17-180126-v1.pdf',
         'ballarat-melton-upgrade/307-Maryborough-temp-timetable-web-19-230126-v1.pdf',
         'ballarat-melton-upgrade/307-Maryborough-temp-timetable-web-110126-v1.pdf',
-      ])[0]
+      ])
+    ], [
+      '--coach-only',
+      '--change-op-day=Monday to Friday=12 & 13 & 14 & 15 & 16 & 19 & 20 & 21 & 22 & 23 January',
+      '--change-op-day=Saturday=17 January',
+      '--change-op-day=Sunday=11 & 18 January',
+      resolvePaths(['geelong-west-tarneit/110-Warrnambool-Temp-BLOB-11-23Jan2026-v1.pdf'])[0]
     ]
   ]
 }
