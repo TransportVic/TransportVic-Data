da1103808e0cb3aec6896655bd2402eb88b0334c
TransportVic <47815427+TransportVic@users.noreply.github.com>
2026-01-12 22:09:37 +1100

    mixed up the file contents whoops

--- gtfs/processors/stop/2/1-set-lil-platform-location.mjs
+++ gtfs/processors/stop/2/1-set-lil-platform-location.mjs
@@ -1,10 +1,8 @@
 export default function processStop(stop) {
-  if (stop.stopGTFSID === '40954') { // FSY Bay 6
-    stop.location.coordinates = [ -37.800964140297744, 144.89999336149893 ].reverse()
-  } else if (stop.stopGTFSID === '21174') { // FSY Bay 4
-    stop.location.coordinates = [ -37.80089847127222, 144.89889056571604 ].reverse()
-  } else if (stop.stopGTFSID === '21019') { // GYW Bay 10
-    stop.location.coordinates = [ -37.879339121184024, 145.1622866072111 ].reverse()
+  if (stop.stopGTFSID === '12164') { // LIL P1
+    stop.location.coordinates = [ -37.757343, 145.345720 ].reverse()
+  } else if (stop.stopGTFSID === '12165') { // LIL P2
+    stop.location.coordinates = [ -37.757306, 145.345646 ].reverse()
   }
 
   return stop
