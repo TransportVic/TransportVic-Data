84e8f9d9a009fa98c25be99bbf9f26580628c740
TransportVic <47815427+TransportVic@users.noreply.github.com>
2026-01-20 10:00:55 +1100

    actually fix springhurst

--- gtfs/processors/stop/1/1-set-springhurst.mjs
+++ gtfs/processors/stop/1/1-set-springhurst.mjs
@@ -1,6 +1,11 @@
 export default function setSpringhurst(stop) {
-  if (stop.stopGTFSID === '1620' && stop.stopName === '') {
+  if (stop.stopGTFSID === '1620' && stop.fullStopName === '') {
     stop.fullStopName = 'Springhurst Railway Station'
+    stop.stopGTFSID = 'vic:rail:SHT'
+  }
+
+  if (stop.stopGTFSID === '22490' && stop.fullStopName === 'Springhurst Railway Station') {
+    stop.parentStopGTFSID = 'vic:rail:SHT'
   }
 
   return stop
--- gtfs/processors/stop/5/3-set-springhurst.mjs
+++ gtfs/processors/stop/5/3-set-springhurst.mjs
@@ -1,5 +1,5 @@
 export default function setSpringhurst(stop) {
-  if (stop.stopGTFSID === '22490' && stop.stopName === 'STOP 1620') {
+  if (stop.stopGTFSID === '22490' && stop.fullStopName === 'STOP 1620') {
     stop.fullStopName = 'Springhurst Railway Station/Silo Street'
   }
 
